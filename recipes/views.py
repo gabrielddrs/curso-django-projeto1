73833aea8d1bc722dd4b36d826eb9925a94bdd57
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2024-05-31 16:13:50 -0300

    feat: Start Site

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from django.urls import path
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, "global/home.html")
-
-def sobre(request):
-    return HttpResponse("Sobre")
-
-def contato(request):
-    return HttpResponse("Contato")
-    
