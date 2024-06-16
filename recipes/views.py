b9a032253d408832e8defe293da675d853a987f9
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2024-06-16 12:02:00 -0300

    updates

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "recipes/pages/home.html")
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        "name":"Gabriel"
+    })
