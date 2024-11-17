79afe9edc4166de313730bba7144192a0098b262
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2024-11-16 22:12:20 -0300

    update recipe views and css

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
-from django.http import Http404
+from django.shortcuts import get_list_or_404, render
 from utils.recipes.factory import make_recipe
 
 from recipes.models import Recipe
@@ -15,26 +14,25 @@ def home(request):
 
 
 def category(request, category_id):
-    #recipes = Recipe.objects.filter(category__id = category_id, is_published = True).order_by('-id')
-
     recipes = get_list_or_404(
         Recipe.objects.filter(
             category__id=category_id,
-            is_published=True
+            is_published=True,
         ).order_by('-id')
     )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes[0].category.name} - Category |'
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
 def recipe(request, id):
-    recipe = get_object_or_404(
+    recipe = Recipe.objects.filter(
         pk=id,
-        is_published=True
-        )
+        is_published=True,
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
